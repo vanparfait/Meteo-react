43190f383ce69b524b4bf08c259294f08776f675
vanparfait <nguemechiaparfait@gmail.com>
2024-05-24 07:32:23 +0100

    mise place et creation du composant de Meteo sans mise en forme

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import loader from "./assets/loader.svg";
+
+function App() {
+  return (
+    <main>
+      <div className="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+      <p className="city-name">Paris</p>
+      <p className="country-name">France</p>
+      <p className="temperature">17</p>
+      <div className="info-icon-container">
+        <img src="./icons/01d.svg" alt="weather-icon" />
+      </div>
+    </main>
+  );
+}
+
+export default App;
