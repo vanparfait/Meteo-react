b77435fa8326fe39610b347ca82b7e046bf47f4e
vanparfait <nguemechiaparfait@gmail.com>
2024-05-25 04:56:17 +0100

    gestion de la partie logique

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,27 @@ import { useEffect, useState } from "react";
 const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
-  const [weatherData, setWeaderData] = useState("null");
+  const [weatherData, setWeaderData] = useState(null);
 
   useEffect(() => {
-    fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
-      .then((response) => response.json())
-      .then((datas) => {
-        console.log(datas);
-        setWeaderData({
-          city: datas.data.city,
-          country: datas.data.country,
-          temperature: datas.data.current.weather.tp,
-          iconId: datas.data.current.weather.ic,
-        });
-      });
+    // Fonction pour récupérer les données de l'API
+    const fetchData = async () => {
+      const response = await fetch(
+        `http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`
+      );
+      console.log(response);
+      const datas = await response.json();
+      console.log(datas);
+
+      if (response.ok) {
+        setWeaderData(datas.data); // Stockage des données dans l'état
+      } else {
+        throw new Error(`API Error: ${datas.status} - ${datas.data.message}`);
+      }
+    };
+
+    fetchData();
   }, []);
-
-  // useEffect(() => {
-  //   // Fonction pour récupérer les données de l'API
-  //   const fetchData = async () => {
-  //     const response = await fetch(
-  //       `http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`
-  //     );
-  //     const datas = await response.json();
-
-  //     if (response.ok) {
-  //       setWeaderData(datas); // Stockage des données dans l'état
-  //     } else {
-  //       throw new Error(`API Error: ${datas.status} - ${datas.data.message}`);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
 
   return (
     <main>
@@ -47,10 +35,10 @@ function App() {
         <>
           <p className="city-name"> {weatherData.city} </p>
           <p className="country-name"> {weatherData.country} </p>
-          <p className="temperature">{weatherData.temperature}°C</p>
+          <p className="temperature">{weatherData.current.weather.tp}°C</p>
           <div className="info-icon-container">
             <img
-              src={`./icons/${weatherData.iconId}.svg`}
+              src={`./icons/${weatherData.current.weather.ic}.svg`}
               alt="weather-icon"
               className="info-icon"
             />
